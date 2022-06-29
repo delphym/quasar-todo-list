a64b7872c6d95fab89dddf7c880a1931c0531df9
Daniel Mladek <daniel_mladek@trimble.com>
2022-06-30 09:58:02 +1200

    added a list

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -3,6 +3,17 @@
     <h5 class="q-mt-none">Help</h5>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec velit mi, congue laoreet rhoncus nec, dignissim eget velit. Sed ultricies diam vel neque rhoncus malesuada. In mollis libero a metus venenatis, sed ultrices tortor auctor. Mauris vitae luctus arcu. Nullam elementum orci orci, non pulvinar erat faucibus nec. Sed pretium ornare dignissim. Etiam sit amet laoreet ipsum. Morbi tempus magna hendrerit sodales interdum. Vestibulum viverra ultricies velit eget ultrices. Aenean dictum pharetra ipsum, eu tempus turpis rhoncus at. Mauris ut tincidunt urna. Nulla posuere hendrerit urna. Proin bibendum metus eu sem eleifend, sed finibus sapien posuere.</p>
     <p>Donec scelerisque dolor et felis maximus, et sodales leo maximus. In hendrerit lacus neque, eu imperdiet mi imperdiet quis. Vestibulum nec lacus a turpis vulputate posuere. Aliquam tortor ipsum, viverra eget metus in, aliquet aliquet mi. Fusce vitae tortor rhoncus lorem lacinia varius sed non ipsum. Sed aliquet, lectus at rhoncus commodo, erat nulla facilisis turpis, vitae aliquam erat mauris ut sapien. Praesent ex turpis, pulvinar nec faucibus lacinia, aliquet vel orci. Suspendisse condimentum efficitur bibendum. Mauris venenatis, erat non sodales dapibus, enim lectus eleifend ligula, et lacinia tortor felis vitae turpis. Duis condimentum purus quis dui ultrices, et auctor est suscipit. Proin ac urna vitae metus vulputate mattis. Donec sed nibh at lacus tempus vulputate. In tincidunt, ex sit amet tristique dignissim, felis magna consequat metus, quis dapibus felis nulla at ex. Donec quis laoreet metus, eget posuere tellus. Morbi tincidunt a neque a tristique.</p>
+    <p>
+      <ol>
+        <li>number:0</li>
+        <li>number:1</li>
+        <li>number:2</li>
+        <li>number:3</li>
+        <li>number:4</li>
+        <li>number:5</li>
+        <li>number:6</li>
+      </ol>
+    </p>
   </q-page>
 </template>
 
